daf946245d9aaa63dbcd73469d6aca754e76ea66
Lucas Spagnoli <lm.spanholi@gmail.com>
2025-09-12 14:39:04 -0300

    Parte do PHP pronta
    Adicionado funções que adicionam gasto, ganho e atualizam a parte de 'resumo'

--- index.php
+++ index.php
@@ -10,8 +10,59 @@
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
-<body>
+<?php
+session_start();
 
+function formatacao($numb)
+{
+    return number_format($numb, 2, ',', '.');
+};
+
+if (!isset($_SESSION['saldo'])) {
+    $_SESSION['saldo'] = [
+        'entradas' => 0,
+        'saidas' => 0,
+        'total' => 0
+    ];
+}
+
+if (!isset($_SESSION['transacoes'])) {
+    $_SESSION['transacoes'] = [];
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $descricao = filter_input(INPUT_POST, 'descricao', FILTER_SANITIZE_SPECIAL_CHARS);
+    $valor = filter_input(INPUT_POST, 'valor', FILTER_SANITIZE_SPECIAL_CHARS);
+    $data = filter_input(INPUT_POST, 'data', FILTER_SANITIZE_SPECIAL_CHARS);
+    $categoria = $_POST['categoria'];
+    $tipo = $_POST['tipo'];
+
+    $transacao = [
+        'descricao' => $descricao,
+        'valor' => (($tipo === 'gasto') && ($valor > 0)) ? ($valor * -1) : ($valor), // Chama a função que formata o número pra 1000 virar 1.000,00, com if ternário que corrije caso algum gasto esteja positivo
+        'data' => $data,
+        'categoria' => $categoria,
+        'tipo' => $tipo
+    ];
+
+    if ($transacao['tipo'] === 'gasto') {
+        $_SESSION['saldo']['saidas'] += $transacao['valor'];
+    } else {
+        $_SESSION['saldo']['entradas'] += $transacao['valor'];
+    }
+
+    $_SESSION['saldo']['total'] = $_SESSION['saldo']['saidas'] + $_SESSION['saldo']['entradas'];
+
+
+    $_SESSION['transacoes'][] = $transacao;
+
+    // Serve pra não reenviar os dados ao recarregar a página:
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit();
+}
+?>
+
+<body>
     <div class="container my-5">
         <div class="bg-header">
             <h1>💰 Calculadora Financeira Pessoal</h1>
@@ -20,23 +71,23 @@
         <div class="card p-4 mt-3">
             <h4>Adicionar Transação</h4>
 
-            <form>
+            <form action="index.php" method="post">
 
                 <div class="row mb-3">
                     <div class="col-md-6">
                         <label for="descricao" class="form-label">Descrição</label>
-                        <input type="text" id="descricao" class="form-control" placeholder="Ex: Supermercado" required>
+                        <input type="text" id="descricao" class="form-control" placeholder="Ex: Supermercado" name="descricao" required>
                     </div>
 
                     <div class="col-md-3">
-                        <label for="valor" class="form-label">Valor (R$)</label>
-                        <input type="number" id="valor" class="form-control" step="0.01" required>
+                        <label for="valor" class="form-label mt-md-0 mt-3">Valor (R$)</label>
+                        <input type="number" id="valor" name="valor" class="form-control" step="0.01" placeholder="0.00" required>
                     </div>
 
                     <div class="col-md-3">
-                        <label for="categoria" class="form-label">Categoria</label>
-
-                        <select id="categoria" class="form-select">
+                        <label for="categoria" class="form-label mt-md-0 mt-3">Categoria</label>
+                        <select id="categoria" class="form-select" name="categoria" required>
+                            <option value="" disabled selected>Escolha...</option>
                             <option value="Alimentação">Alimentação</option>
                             <option value="Transporte">Transporte</option>
                             <option value="Lazer">Lazer</option>
@@ -49,15 +100,23 @@
                 <div class="row mb-3">
                     <div class="col-md-4">
                         <label for="data" class="form-label">Data</label>
-                        <input type="date" id="data" class="form-control" required>
+                        <input name="data" type="date" id="data" class="form-control" required>
+                    </div>
+
+                    <div class="col-md-3">
+                        <label for="categoria" class="form-label mt-md-0 mt-3">Categoria</label>
+                        <select id="categoria" class="form-select" name="tipo" required>
+                            <option value="" disabled selected>Tipo...</option>
+                            <option value="gasto">Gasto</option>
+                            <option value="ganho">Ganho</option>
+                        </select>
                     </div>
                 </div>
 
                 <button type="submit" class="btn btn-custom">Adicionar</button>
-
             </form>
-        </div
-        >
+        </div>
+
         <div class="card p-4 mt-4">
             <h4>Transações</h4>
 
@@ -84,6 +143,19 @@
                         <td>Outros</td>
                         <td>2025-09-05</td>
                     </tr>
+
+                    <?php
+                    if ($_SESSION['transacoes']) {
+                        foreach ($_SESSION['transacoes'] as $transacao):
+                            echo '<tr>';
+                            echo '<td>' . $transacao['descricao'] . '</td>';
+                            echo '<td> R$ ' . formatacao($transacao['valor']) . '</td>';
+                            echo '<td>' . $transacao['categoria'] . '</td>';
+                            echo '<td>' . $transacao['data'] . '</td>';
+                            echo '</tr>';
+                        endforeach;
+                    }
+                    ?>
                 </tbody>
 
             </table>
@@ -96,26 +168,26 @@
                 <div class="col-md-4">
                     <div class="alert alert-success text-center">
                         <h5>Entradas</h5>
-                        <p><strong>R$ 3.500,00</strong></p>
+                        <p><strong><?php echo 'R$' . formatacao($_SESSION['saldo']['entradas'])?></strong></p>
                     </div>
                 </div>
 
                 <div class="col-md-4">
                     <div class="alert alert-danger text-center">
                         <h5>Saídas</h5>
-                        <p><strong>R$ 120,50</strong></p>
+                        <p><strong><?php echo 'R$' . formatacao($_SESSION['saldo']['saidas']) ?></strong></p>
                     </div>
                 </div>
 
                 <div class="col-md-4">
                     <div class="alert alert-info text-center">
                         <h5>Saldo</h5>
-                        <p><strong>R$ 3.379,50</strong></p>
+                        <p><strong><?php echo 'R$' . formatacao($_SESSION['saldo']['total']) ?></strong></p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
 </body>
+
 </html>
